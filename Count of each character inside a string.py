35d07bdc530c1d857b9e1ecda2ddd8c290bfcfbc
Tajamul Khan <tajamulk@gmail.com>
2024-08-09 09:47:06 +0530

    Update Count of each character inside a string.py

--- Count of each character inside a string.py
+++ Count of each character inside a string.py
@@ -1,3 +1,5 @@
+# Using pprint
+
 import pprint  #The pprint module's pprint() "pretty print" function can display a dictionary value cleanly
 
 m = 'a big fat hen'
@@ -9,3 +11,11 @@ for character in m:
     count[character] += 1
 
 pprint.pprint(count)
+
+# Using Counter
+
+from collections import Counter
+
+m = 'a big fat hen'
+count = Counter(m)
+print(count)
