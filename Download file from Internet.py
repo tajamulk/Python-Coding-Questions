4c588146f5b4c93a104517263c258970eb8269dc
Tajamul Khan <tajamulk@gmail.com>
2024-08-10 07:18:27 +0530

    Update Download file from Internet.py

--- Download file from Internet.py
+++ Download file from Internet.py
@@ -9,3 +9,9 @@ response = requests.get(url)
 print(len(response.text))
 print(response.raise_for_status)  # Resonse 200 means OK, while 404 is error
 print(response.text[:550])
+
+# Saving the above downloaded file
+
+downloadfile = open('gutenbergfile', 'wb') #wb = write binary
+for chunk in response.iter_content(100000):
+    downloadfile.write(chunk)
