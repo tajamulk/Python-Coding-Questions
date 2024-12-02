f17a04b7a3403c75292987cb5a286b15f6c368a2
Tajamul Khan <tajamulk@gmail.com>
2024-12-03 01:32:26 +0530

    add

--- Python Web Browser.py
+++
@@ -1,6 +0,0 @@
-import webbrowser
-
-address = input("Please enter the address: ")
-
-# Assuming you want to open this address in a web browser
-webbrowser.open(f'https://www.google.com/maps/place/{address}')
